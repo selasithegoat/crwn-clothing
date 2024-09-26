c78b16b1fb1a9d9015f62ed12d6e1444994eb62e
selasithegoat <enochdoe355@gmail.com>
2024-09-25 21:29:05 -0400

    udate our menu-items to navigate using useNavigate hooks

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,29 @@
-import React from "react";
-import "./menu-item.styles.scss";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate(); // Using the useNavigate hook
+
+  const handleClick = () => {
+    navigate(linkUrl); // Navigate to the provided linkUrl
+  };
+
+  return (
     <div
-      className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={handleClick} // Call handleClick on click
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import Directory from "../../components/directory/directory.component";
+import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
-
 
 const HomePage = () => (
   <div className="homepage">
