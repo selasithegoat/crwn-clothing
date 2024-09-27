4d5161a7604789fd0f196ca4ec8d7b969cb8d254
selasithegoat <enochdoe355@gmail.com>
2024-09-26 21:51:24 -0400

    Created a sign in page, sign-in component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
